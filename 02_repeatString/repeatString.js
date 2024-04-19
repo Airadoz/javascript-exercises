984f2ff3bfe1947ea26df1991b0894c1b7841ecc
Airadoz <Airadozik@gmail.com>
2024-04-19 21:43:51 +0500

    Passed 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (string, number) {
+  let result = "";
 
+  if (number < 0) return "ERROR";
+  if (number === 0) return "";
+  if (number > 0) {
+    for (i = 0; i < number; i++) {
+      result += string;
+    }
+  }
+
+  return result;
 };
 
 // Do not edit below this line
